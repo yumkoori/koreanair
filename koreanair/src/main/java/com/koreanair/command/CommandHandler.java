7df27ec97003c723d0de14abfa7b42d3dc694b1a
yumi <yumkoori@gmail.com>
2025-06-11 15:17:45 +0900

    DispatcherServlet 추가

---
+++ koreanair/src/main/java/com/koreanair/command/CommandHandler.java
@@ -0,0 +1,8 @@
+package com.koreanair.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface CommandHandler {
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception;
+}
